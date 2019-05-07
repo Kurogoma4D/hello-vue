5daf1cfc2961258cf71145a139eb4308bd77a480
Kurogoma4D <highdro0479@gmail.com>
2019-05-08 00:56:48 +0900

    add: toggle button

---
+++ src/toggleButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button
+      v-bind:class="{ selected: buttonSelected }"
+      v-on:click="buttonSelected = !buttonSelected"
+    >Toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      buttonSelected: false
+    };
+  }
+};
+</script>
+<style scoped>
+button {
+  padding: 10px 30px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
+  outline: 0;
+}
+
+button.selected {
+  background-color: #6fa;
+  border-color: #6fa;
+}
+</style>
